e45565c79b189b20c0544a763552c7807727c435
ZY <460710283@qq.com>
2015-08-10 17:13:53 +0800

    the second

---
+++ TableViewCell/TableViewCell/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  TableViewCell
+//
+//  Created by huangbing on 15/8/10.
+//  Copyright (c) 2015年 hb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewCell/TableViewCell/CustomTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.m
+//  TableViewCell
+//
+//  Created by huangbing on 15/8/10.
+//  Copyright (c) 2015年 hb. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
